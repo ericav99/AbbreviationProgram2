0de24e57acadd3f31ec95e93d4628b4b01865f24
ericav99 <38194054+ericav99@users.noreply.github.com>
2018-09-15 16:33:13 -0700

    Add files via upload

---
+++ client.cpp
@@ -0,0 +1,67 @@
+// Testing Client Program for class Key
+// filename: testingclient.cpp
+
+#include "key.h"
+#include "item.h"
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+   Key firstOne;
+   Key secondOne;
+   ifstream InFile;
+   ofstream OutFile;
+   int convertedNumber;
+   
+   cout << endl << endl;    // to move first output away from command prompt 
+   
+   cout << "          Testing the constructor" << endl;
+   cout << "string is empty and show nothing between # signs" << endl;
+   cout << "#";
+   firstOne.write(cout);//displays the empty string textingAbbreviation
+   cout << "#" << endl;
+   cout << endl;
+   
+   cout << "          Testing the read from a user and write to the screen " << endl;
+   cout << "Enter a texting abbreviations -> ";  // note the arrow to show the user where to type and left on same line
+   firstOne.read(cin); // user input to textingAbbreviation
+   cout << "shows result between # signs" << endl;
+   cout << "#";
+   firstOne.write(cout);// displays the user inputed textingAbbreviation
+   cout << "#" << endl;
+   cout << endl;
+   
+   cout << "Testing the read from a file and write to a file "<<endl;  // note the arrow to show the user where to type and left on same line
+   cout<< "Reading from the file...";
+   InFile.open("in.dat");//opens the in.dat file
+   firstOne.read(InFile);//reads the in.dat file into the textingAbbreviation of instance firstOne.
+   OutFile.open("out.dat");//opens the out.dat file
+   cout<< "writing in.dat to out.dat "<< endl; 
+   firstOne.write(OutFile);//writes the textingAbbreviation of instance firstOne into the out.dat file
+   cout << endl<<endl;
+   
+   cout << "Testing the convertToInteger and shows result to the screen between # signs" << endl;
+   cout << "#";
+   cout<< firstOne.convertToInteger(); // calls the convertToInteger function for the instance firstOne. 
+   //Will display the sum of the ascii numbers corresponding to each character in the string textingAbbreviation
+   cout<< "#";
+   cout<< endl<<endl;
+   
+   cout<< "Testing the assign function " <<endl;
+   cout << "Showing the first textingAbbreviation between # signs" << endl;
+   cout << "#";
+   firstOne.write(cout);//displays the textingAbbreviation from instance firstOne.
+   cout<< "#";
+   cout<<endl<<endl;
+   cout<<" Assigning the firstOne to the secondOne "<<endl;
+	secondOne.assign(firstOne);//Copies the textingAbbreviation from instance firstOne over to the empty textingAbbreviation of instance secondOne
+	cout<<endl;
+	cout << "Showing that the secondOne got assigned between # signs" << endl;
+   cout << "#";
+   secondOne.write(cout);//Shows that the textingAbbreviation in instance secondOne matches that from instance firstOne
+   cout<< "#"<<endl;
+   
+   return 0;
+}
---
+++ item.cpp
@@ -0,0 +1,77 @@
+// Specification of ADT Item wherex
+//     an Item is-a Key  (technical computer science)
+//     class Item is a derived or child class of class Key
+
+//     data object: a meaning of a texting abbreviation
+//     operations: create, destroy, copy create, copy
+//                 input, output
+
+#include "key.h" 
+#include "item.h"
+
+istream& operator>> (istream& input, Item& rightHandSideItem)
+{
+	input>> rightHandSideItem.textingAbbreviation;
+	input.get(newline);
+	getline(input, rightHandSideItem.meaning);
+	return input;
+}
+ 
+//allows for the output of an item to the standard output device or a file
+//pre rightHandSideItem has been filled with a texting abbreviation and
+//    its associated meaning 
+//    output has been opened
+//post rightHandSideItem has been written to the output device
+//     lol:  laugh out loud
+// 
+//usage  outfile << myItem;
+ostream& operator<< (ostream& output, const Item& rightHandSideItem)
+{
+output << rightHandSideItem.textingAbbreviation;
+cout<< " means: ";
+output << rightHandSideItem.meaning;
+return output;	
+}
+
+
+//creates an empty item (texting abbreviation and its associated meaning)
+//post 
+//usage Item aitem;
+Item::Item()
+{
+
+}
+
+
+//releases memory for a texting abbreviation and its associated meaning
+//pre Item object exists
+//post Item object no longer exists, memory has been freed
+//usage automatically done at the end of scope
+Item::~Item()
+{
+	
+}
+
+//creates a new item and copies the right hand side item into item
+//pre
+//post
+//usage  Item myItem(yourItem);
+Item::Item(const Item& rightHandSideItem)
+{
+Item newObject = rightHandSideItem;
+}
+
+//
+//pre
+//post
+//usage
+Item& operator=(const Item& rightHandSideItem)
+{
+	if (this != &rightHandSideKey)
+	{
+       textingAbbreviation = rightHandSideKey.textingAbbreviation;
+	   meaning = rightHandSideKey.meaning;
+	}
+	return *this;
+}
+      
